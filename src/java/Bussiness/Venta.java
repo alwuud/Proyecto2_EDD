30acc26556e98c0b25c59ddc89768bcfb0d3f667
alwuud <dahh.drag@gmail.com>
2016-07-05 11:45:00 -0600

    primer master

---
+++ src/java/Bussiness/Venta.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bussiness;
+
+/**
+ *
+ * @author daniel
+ */
+public class Venta {
+    
+    private int factura;
+    private double total;
+    private String fecha;
+    
+    
+   
+    
+}
