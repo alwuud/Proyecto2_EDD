3271c7e86ea4edd46a9b432ddabe6141587ad74e
alwuud <dahh.drag@gmail.com>
2016-07-12 19:47:42 -0600

    Ulitmo

--- src/java/Structures/Hash/Tabla.java
+++ src/java/Structures/Hash/Tabla.java
@@ -5,10 +5,34 @@
  */
 package Structures.Hash;
 
+import Bussiness.Producto;
+
 /**
  *
  * @author daniel
  */
 public class Tabla {
     
+    private Producto productos[];
+    private boolean empty[];
+    private double factorCarga;
+        
+    
+    int tamanoActual;
+    
+    public Tabla(){
+        productos= new Producto[23];
+        empty= new boolean[23];
+        
+    }
+    
+    
+    
+    public int hashFunction(int clave){
+        
+        
+        return 0;
+        
+        
+    }
 }
